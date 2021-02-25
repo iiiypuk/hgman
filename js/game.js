a87d2c7f75ef2387aba289dd5212f743bda433cf
Alexander Popov <iiiypuk@iiiypuk.me>
2021-02-25 07:46:59 +0300

    separate js

--- js/game.js
+++ js/game.js
@@ -6,18 +6,6 @@ const keyboardLayouts = {
 	'ruQwertyKeyboard' : 'йцукеёнгшщзхъфывапролджэячсмитьбю'.toUpperCase()
 }
 
-const pageCredits = '\
-	<p class="text-center">Font F5.6 by\
-	  <a href="http://dotcolon.net/">DOT COLON</a>\
-  </p>\
-  \
-  <p class="text-center">UI by\
-  	<a href="https://jenil.github.io/chota/">chota.css</a>\
-  </p>\
-';
-
-const pageStatistics = '';
-
 const words = [
 	'google', 'speed', 'design', 'forest', 'forever', 'love',
 	'horizon', 'defect'
