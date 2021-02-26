0addb4d4dcf62cca565148287fec39fbca83d7aa
Alexander Popov <iiiypuk@iiiypuk.me>
2021-02-27 02:38:38 +0300

    beautify js

--- js/_statFunction.js
+++ js/_statFunction.js
@@ -1,38 +1,42 @@
-'use strict';
+"use strict";
 
-function updateStats(statParameter)
-{
-	let stTotalGames = JSON.parse(localStorage.getItem('stTotalGames'));
-	let stWinWords = JSON.parse(localStorage.getItem('stWinWords'));
-	let stLetterClick = JSON.parse(localStorage.getItem('stLetterClick'));
-	let stCorrLetter = JSON.parse(localStorage.getItem('stCorrLetter'));
+function updateStats(statParameter) {
+    let stTotalGames = JSON.parse(localStorage.getItem("stTotalGames"));
+    let stWinWords = JSON.parse(localStorage.getItem("stWinWords"));
+    let stLetterClick = JSON.parse(localStorage.getItem("stLetterClick"));
+    let stCorrLetter = JSON.parse(localStorage.getItem("stCorrLetter"));
+    switch (statParameter) {
+      case "stTotalGames":
+        localStorage.setItem("stTotalGames", stTotalGames + 1);
+        break;
 
-	switch(statParameter)
-	{
-		case 'stTotalGames':
-			localStorage.setItem('stTotalGames', stTotalGames + 1);
-			
-			break;
-		case 'stWinWords':
-			localStorage.setItem('stTotalGames', stTotalGames + 1);
-			localStorage.setItem('stWinWords', stWinWords + 1);
-			
-			break;
-		case 'stLetterClick':
-			localStorage.setItem('stLetterClick', stLetterClick + 1);
-			
-			break;
-		case 'stCorrLetter':
-			localStorage.setItem('stLetterClick', stLetterClick + 1);
-			localStorage.setItem('stCorrLetter', stCorrLetter + 1);
-			
-			break;
-		case 'stCheck':
-			if (stTotalGames == null) { localStorage.setItem('stTotalGames', 0); }
-			if (stWinWords == null) { localStorage.setItem('stWinWords', 0); }
-			if (stLetterClick == null) { localStorage.setItem('stLetterClick', 0); }
-			if (stCorrLetter == null) { localStorage.setItem('stCorrLetter', 0); }
+      case "stWinWords":
+        localStorage.setItem("stTotalGames", stTotalGames + 1);
+        localStorage.setItem("stWinWords", stWinWords + 1);
+        break;
 
-			break;
-	}
+      case "stLetterClick":
+        localStorage.setItem("stLetterClick", stLetterClick + 1);
+        break;
+
+      case "stCorrLetter":
+        localStorage.setItem("stLetterClick", stLetterClick + 1);
+        localStorage.setItem("stCorrLetter", stCorrLetter + 1);
+        break;
+
+      case "stCheck":
+        if (stTotalGames == null) {
+            localStorage.setItem("stTotalGames", 0);
+        }
+        if (stWinWords == null) {
+            localStorage.setItem("stWinWords", 0);
+        }
+        if (stLetterClick == null) {
+            localStorage.setItem("stLetterClick", 0);
+        }
+        if (stCorrLetter == null) {
+            localStorage.setItem("stCorrLetter", 0);
+        }
+        break;
+    }
 }
